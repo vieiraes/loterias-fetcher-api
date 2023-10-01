f0f27a270c04d5a55ea19d3213747c3188e0f586
Bruno Vieira <vieira.es@gmail.com>
2023-10-01 12:47:16 -0300

    feature:added route to capture lottery

--- src/controllers/Lottery.controller.ts
+++ src/controllers/Lottery.controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from 'express'
 import express from 'express'
+import { mountUrl } from '../fetch'
+import axios from 'axios'
+require('dotenv').config()
 
 
 const router = express.Router()
@@ -7,18 +10,33 @@ const router = express.Router()
 router.post('/capture', (req: Request, res: Response) => {
 
     try {
-        const { lottery, initialCount, endCount }: { lottery: string, initialCount: number, endCount: number } = req.body
-
-        !initialCount ? res.status(400).send({ message: 'falta initialCount' }) : undefined
-
-        const objeto = {
-            data: {
-                "lottery": lottery,
-                "initialCount": initialCount,
-                "endCount": endCount
+        const { lottery, concurso }: { lottery: string, concurso: string } = req.body
+        async function run() {
+            const url: string = `https://apiloterias.com.br/app/resultado?loteria=${lottery}&token=${process.env.TOKEN}&concurso=${concurso}`
+            let dezenas: string
+            try {
+                const res = await axios.get(url).then(res => {
+                    dezenas = res.data.dezenas.toString()
+                    return dezenas
+                })
+                return res
+            } catch (error) {
+                console.error('Erro:', error)
             }
+
         }
-        res.status(201).send(objeto)
+
+
+        run().then((dezenas) => {
+            const objeto = {
+                data: {
+                    "lottery": lottery,
+                    "concurso": concurso,
+                    "dezenas": dezenas
+                }
+            }
+            res.status(201).send(objeto)
+        })
     } catch (error) {
         res.status(500).json({
             message: { error: error }
--- src/fetch.ts
+++ src/fetch.ts
@@ -2,29 +2,25 @@ require('dotenv').config()
 import axios from 'axios'
 import fs from 'fs'
 
-import { router } from './api'
-
-
 function criaArray(): string[] {
     let concInicial: number = 1
-    let concFinal: number = 2915
+    let concFinal: number = 100
     let arrayConcursos: string[] = []
     for (let index: number = concInicial; index <= concFinal; index++) {
         arrayConcursos.push(index.toString())
     }
-    return arrayConcursos;
+    return arrayConcursos
 }
 let arrayConcurso: string[] = criaArray()
 
 
-
-async function mountUrl(concursoNumber: string): Promise<string> {
+export async function mountUrl(concursoNumber: string) {
     const loteria: string = "lotofacil"
     const url: string = `https://apiloterias.com.br/app/resultado?loteria=${loteria}&token=${process.env.TOKEN}&concurso=${concursoNumber}`
     return url
 }
 
-async function fetchData(route: string): Promise<string> {
+export async function fetchData(route: string): Promise<string> {
     try {
         const res = await axios.get(route)
         return res.data.dezenas.toString()
@@ -33,7 +29,7 @@ async function fetchData(route: string): Promise<string> {
         throw error
     }
 }
-function gravarnoarquivo(data) {
+function gravaNoArquivo(data: string) {
     const filename = 'dados.txt'
     try {
         fs.writeFileSync(filename, data, 'utf-8')
@@ -45,15 +41,14 @@ function gravarnoarquivo(data) {
 }
 
 
-async function run() {
-
+export async function run() {
     let arrayMatriz: string[] = []
     for (const concurso of arrayConcurso) {
         const url = await mountUrl(concurso);
         try {
             const result = await fetchData(url)
             arrayMatriz.push(result)
-            console.log(result);
+            console.log(result)
         } catch (error) {
             console.error('Erro:', error);
         }
@@ -61,12 +56,12 @@ async function run() {
     return arrayMatriz
 }
 
-run().then((arrayMatriz) => {
-    console.log(arrayMatriz);
-    const resultado = gravarnoarquivo(arrayMatriz.join('\n')); // Junte os dados com quebras de linha
-    if (resultado) {
-        console.log('Dados gravados com sucesso.');
-    } else {
-        console.error('Erro ao gravar os dados.');
-    }
-});
+// run().then((arrayMatriz) => {
+//     console.log(arrayMatriz);
+//     const resultado = gravaNoArquivo(arrayMatriz.join('\n'))
+//     if (resultado) {
+//         console.log('Dados gravados com sucesso.');
+//     } else {
+//         console.error('Erro ao gravar os dados.');
+//     }
+// })
